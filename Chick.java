3d306a625ba4fe9a2f30bda791873373704c6ed8
Tidang <tidang1@s.sfusd.edu>
2018-10-23 13:38:04 -0700

    Hello

--- Chick.java
+++ Chick.java
@@ -2,17 +2,27 @@ class Chick implements Animal
 {     
      private String myType;
      private String mySound;
+
+    
      
-     public Chick(String type, String sound){
+     public Chick(String type, String soundOne, String soundTwo){
       myType = type;
-      mySound = sound;
+      
+      if((Math.random() > .5)){
+      mySound = soundOne;
+      }else{
+      mySound = soundTwo;
+      }
      }
      
      public Chick(){
       myType = "unknown";
       mySound = "unknown";
+    
      }
      
+
+
+      public String getSound(){return mySound;} 
      public String getType(){return myType;}
-     public String getSound(){return mySound;}
 }
--- Farm.java
+++ Farm.java
@@ -2,8 +2,8 @@ class Farm
 {     
    private Animal [] aBunchOfAnimals = new Animal [3];
    public Farm() {
-    aBunchOfAnimals[0] = new Cow("cow", "moo");
-    aBunchOfAnimals[1] = new Chick("chick", "cluck");
+    aBunchOfAnimals[0] = new NamedCow("cow", "Elsie", "moo");
+    aBunchOfAnimals[1] = new Chick("chick", "cluck", "cheep");
     aBunchOfAnimals[2] = new Pig("pig", "oink");
    }
    
@@ -11,5 +11,6 @@ class Farm
      for (int nI = 0; nI < aBunchOfAnimals.length; nI++){
        System.out.println(aBunchOfAnimals[nI].getType() + " goes " + aBunchOfAnimals[nI].getSound());
     }
+        System.out.println("The cow is known as " + ((NamedCow)aBunchOfAnimals[0]).getName() );
    }
 }
